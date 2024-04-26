84cd26cfdd7795e57be9775fc4078664cce4f48c
oem <boom.rezonans@yandex.ru>
2024-04-26 16:24:57 +0300

    Доработка

--- client.py
+++ client.py
@@ -20,11 +20,12 @@ IFF_NO_PI = 0x1000
 parser = argparse.ArgumentParser(description='Process file types list.')
 parser.add_argument('--IP-dst', type=str, default='192.168.1.1', help='IP address. Default 192.168.1.1')
 parser.add_argument('--Eth-dst', type=str, default='0a:1a:de:3c:f0:5d', help='Ether address. Default 0a:1a:de:3c:f0:5d')
-parser.add_argument('--data', type=str, default='Firts_default', help='Input your test text')
+parser.add_argument('--TCP-port', type=int, default=12345, help='input TCP port')
 args = parser.parse_args()
 IP_dst = args.IP_dst
 Eth_dst = args.Eth_dst
-Test_text = args.data
+TCP_port = args.TCP_port
+Test_text = 'Hello world'
 
 tap = open('/dev/net/tun', 'r+b', buffering=0)
 ifr = struct.pack('16sH', b'tap0', IFF_TAP | IFF_NO_PI)
@@ -38,23 +39,27 @@ subprocess.check_call('ifconfig tap0 192.168.1.2 pointopoint 192.168.1.1 up', sh
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    client_socket.connect((server_ip, server_port))
+    for _ in range(3):
+        client_socket.connect((server_ip, server_port))
 
-    packet = Ether(dst=f'{Eth_dst}', src = f'{Eth_dst}') / IP(dst = f'{IP_dst}', src= f'{IP_dst}') / TCP(dport = 12345, sport = 54321) / Raw(load = f'{Test_text}')
+        while True:
+            Test_text = input('Enter the text to send to the interface:\n')
 
-    while True:
-        # Read data from the TAP interface
-        sendp(packet, iface='tap0')
-        packet = os.read(tap.fileno(), 2048)
-        print('Packet read from TAP interface:', packet)
+            packet = Ether(dst=f'{Eth_dst}', src=f'{Eth_dst}') / IP(dst=f'{IP_dst}', src=f'{IP_dst}') / TCP(dport=12345,
+                                                                                                            sport=54321) / Raw(
+                load=f'{Test_text}')
 
-        # Send the data to the server
-        client_socket.sendall(packet)
+            # Read data from the TAP interface
+            sendp(packet, iface='tap0')
+            packet = os.read(tap.fileno(), 2048)
+            print('Packet read from TAP interface:', packet)
 
-        # Receive response from the server
-        server_response = client_socket.recv(1024).decode()
-        print(f"Server response: {server_response}")
+            # Send the data to the server
+            client_socket.sendall(packet)
 
+            # Receive response from the server
+            server_response = client_socket.recv(1024).decode()
+            print(f"Server response: {server_response}")
 
 except ConnectionRefusedError:
     print("Connection to the server refused. Make sure the server is running.")
