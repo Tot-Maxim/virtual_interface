1420d97e7167ee155c0b5678a745b2d58eed6f72
tot <boom.rezonans@yandex.ru>
2024-05-09 18:44:36 +0300

    Version 1.0.0

--- code_for_send.py
+++ code_for_send.py
@@ -1,20 +1,20 @@
+from scapy.layers.l2 import Ether
+from scapy.layers.inet import IP, ICMP
+from scapy.packet import Raw
 from scapy.all import *
 from array import array
 import fcntl
 import os
 import struct
 import subprocess
-import tempfile
 import time
 import threading
 import select
-
 
 try:
     subprocess.check_call('sudo ip route delete 10.0.0.0/7 dev tap0 proto kernel scope link src 10.1.1.7', shell=True)
 except:
     pass
-
 
 state = 1
 temp_read = b''
@@ -24,22 +24,9 @@ DST_IP = '10.1.1.8'
 SRC_IP = '10.1.1.7'
 #CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 CURRENT_DIR = '/home/tot/FilePack'
-PACK = Ether(dst="0a:1a:de:3c:f0:5d", src="0a:1a:de:3c:f0:5d") / IP(dst=DST_IP, src=SRC_IP) / ICMP(type="echo-request") / Raw(load='Check connect to host')
-TUNSETIFF = 0x400454ca
-TUNSETOWNER = TUNSETIFF + 2
-IFF_TUN = 0x0001
-IFF_TAP = 0x0002
-IFF_NO_PI = 0x1000
+PACK = Ether(dst="0a:1a:de:3c:f0:5d", src="0a:1a:de:3c:f0:5d") / IP(dst=DST_IP, src=SRC_IP) / ICMP(
+    type="echo-request") / Raw(load='Check connect to host')
 read_lock = threading.Lock()
-
-# Открытие файла, соответствующего устройству TUN, в двоичном режиме чтения/записи без буферизации.
-tun = open('/dev/net/tun', 'r+b', buffering=0)
-ifr = struct.pack('16sH', b'tap0', IFF_TAP | IFF_NO_PI)
-fcntl.ioctl(tun, TUNSETIFF, ifr)
-fcntl.ioctl(tun, TUNSETOWNER, 1000)
-
-# Поднятие tap0 и назначение адреса
-subprocess.check_call(f'ifconfig tap0 {SRC_IP} pointopoint {DST_IP} up', shell=True)
 
 
 class bcolors:
@@ -54,83 +41,85 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-class AtomicWrite():
-    def __init__(self, path, mode='w', encoding='utf-8'):
-        self.path = path
-        self.mode = mode if mode == 'wb' else 'w'
-        self.encoding = encoding
+class TAP_Manager:
+    def __init__(self):
+        self.file_from_host = file_from_host
+        self.file_from_virtual = file_from_virtual
+        self.temp_read = temp_read
+        self.src_ip = SRC_IP
+        self.dst_ip = DST_IP
+        self.read_lock = threading.Lock()
+        self.tun_setup()
 
-    def __enter__(self):
-        self.temp_file = tempfile.NamedTemporaryFile(
-            mode=self.mode,
-            encoding=self.encoding if self.mode != 'wb' else None,
-            delete=False
-        )
-        return self.temp_file
+    def tun_setup(self):
+        TUNSETIFF = 0x400454ca
+        TUNSETOWNER = TUNSETIFF + 2
+        IFF_TUN = 0x0001
+        IFF_TAP = 0x0002
+        IFF_NO_PI = 0x1000
 
-    def __exit__(self, exc_type, exc_message, traceback):
-        self.temp_file.close()
-        if exc_type is None:
-            os.rename(self.temp_file.name, self.path)
-            os.chmod(self.path, 0o664)
-        else:
-            os.unlink(self.temp_file.name)
-            print(bcolors.FAIL + "Break")
+        self.tun_in = open('/dev/net/tun', 'r+b', buffering=0)
+        ifr = struct.pack('16sH', b'tap0', IFF_TAP | IFF_NO_PI)
+        fcntl.ioctl(self.tun_in, TUNSETIFF, ifr)
+        fcntl.ioctl(self.tun_in, TUNSETOWNER, 1000)
+        subprocess.check_call(f'ifconfig tap0 {self.src_ip} pointopoint {self.dst_ip} up', shell=True)
+        return self.tun_in
 
+    def write_packet_to_file(self, packet, file_path):
+        lock_file_path = file_path + '.lock'
 
-def write_packet_to_file(packet, file_path):
-    lock_file_path = file_path + '.lock'
+        for _ in range(2):
+            try:
+                with open(lock_file_path, 'w') as lock_file:
+                    fcntl.flock(lock_file, fcntl.LOCK_EX)  # Acquire an exclusive lock on the lock file
 
-    for _ in range(2):
-        try:
-            with open(lock_file_path, 'w') as lock_file:
-                fcntl.flock(lock_file, fcntl.LOCK_EX)  # Acquire an exclusive lock on the lock file
-
-                with open(file_path, 'ab+') as file:
-                    file.write(packet)
-                    file.write(b'\t0t')
-                    print(bcolors.WARNING + f'Write data in {file_path}: ' + bcolors.ENDC,
-                          ''.join('{:02x} '.format(x) for x in packet))
+                    with open(file_path, 'ab+') as file:
+                        file.write(packet)
+                        file.write(b'\t0t')
+                        print(bcolors.WARNING + f'Write data in {file_path}: ' + bcolors.ENDC,
+                              ''.join('{:02x} '.format(x) for x in packet))
                 fcntl.flock(lock_file, fcntl.LOCK_UN)  # Release the lock on the lock file
-        except Exception as e:
-            time.sleep(0.1)
-            print(f"Failed to write to file: {e}. Retrying...")
-            continue
-        finally:
-            if os.path.exists(lock_file_path):
-                os.remove(lock_file_path)
-                return True
-    return False
+            except:
+                time.sleep(0.1)
+                continue
+            finally:
+                if os.path.exists(lock_file_path):
+                    os.remove(lock_file_path)
+                    return True
+        return False
 
+    def read_packet(self, path_dir):
+        global temp_read
 
-def read_packet(path_dir):
-    global temp_read
-
-    if not os.path.exists(path_dir):
-        print('Server not found')
-        return True
-
-    with open(path_dir, 'rb+') as file:
-        content = file.read()
-        index = content.find(b'\t0t')
-
-        if index != -1:
-            to_TCP = content[:index]
-            after_content = content[index + 3:]
-
-            if temp_read != to_TCP:
-                temp_read = to_TCP
-                print(bcolors.OKGREEN + f'Read_data in {path_dir}:' + bcolors.ENDC, ''.join('{:02x} '.format(x) for x in to_TCP))
-                os.write(tun.fileno(), bytes(to_TCP))
-                return False
-            else:
-                pass
-            file.seek(0)
-            file.truncate()
-            file.write(after_content)
-        else:
+        if not os.path.exists(path_dir):
+            print('Server not found')
             return True
 
+        with open(path_dir, 'rb+') as file:
+            content = file.read()
+            index = content.find(b'\t0t')
+
+            if index != -1:
+                to_TCP = content[:index]
+                after_content = content[index + 3:]
+
+                if temp_read != to_TCP:
+                    temp_read = to_TCP
+                    print(bcolors.OKGREEN + f'Read_data in {path_dir}:' + bcolors.ENDC,
+                          ''.join('{:02x} '.format(x) for x in to_TCP))
+                    os.write(self.tun_in.fileno(), bytes(to_TCP))
+                    return False
+                else:
+                    pass
+                file.seek(0)
+                file.truncate()
+                file.write(after_content)
+            else:
+                return True
+
+
+tap_manager = TAP_Manager()
+tun = tap_manager.tun_setup()
 
 while True:
     print('1', state)
@@ -156,13 +145,12 @@ while True:
     print('2', state)
     if state == 2:
         path_dir = os.path.join(CURRENT_DIR, file_from_host)
-        if write_packet_to_file(from_TCP, path_dir):
+        if tap_manager.write_packet_to_file(from_TCP, path_dir):
             state = 3
     print('3', state)
     if state == 3:
         path_dir = os.path.join(CURRENT_DIR, file_from_virtual)
-        if read_packet(path_dir):
+        if tap_manager.read_packet(path_dir):
             state = 1
 
 tun.close()
-
