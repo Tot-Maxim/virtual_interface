381babc7ba78f58e586e3c2cadcda5f8130b5afd
oem <boom.rezonans@yandex.ru>
2024-05-16 14:10:46 +0300

    select

--- daemon_tap.py
+++ daemon_tap.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from mytuntap import TAP_Manager
 import threading
+import select
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description='TAP Manager Script')
-parser.add_argument('--current_dir', type=str, default='/home/tot/FilePack',
+parser.add_argument('--current_dir', type=str, default=os.getcwd(),
                     help='Current directory path')
 parser.add_argument('--file_from_host', type=str, default='from_host', help='File from host')
 parser.add_argument('--file_from_virtual', type=str, default='from_virtual', help='File from virtual')
@@ -17,12 +19,22 @@ args = parser.parse_args()
 
 # Инициализация класса TAP_Manager
 tap_manager = TAP_Manager(args.src_ip, args.dst_ip)
-tap_lock = threading.Lock()
+tun = tap_manager.tun_setup()
 
-read_thread = threading.Thread(target=tap_manager.read_from_file,
-                               args=(tap_lock, args.current_dir, args.file_from_virtual))
-write_thread = threading.Thread(target=tap_manager.read_from_TCP, args=(tap_lock, args.current_dir,
-                                                                        args.file_from_host))
+while True:
+    read_tun, write_tun, _ = select.select([tun.fileno()], [tun], [])
+    if tun.fileno() in read_tun:
+        tap_manager.read_from_TCP(args.current_dir, args.file_from_host)
 
-read_thread.start()
-write_thread.start()
+    if tun in write_tun:
+        tap_manager.read_from_file(args.current_dir, args.file_from_virtual)
+
+# tap_lock = threading.Lock()
+#
+# read_thread = threading.Thread(target=tap_manager.read_from_file,
+#                                args=(tap_lock, args.current_dir, args.file_from_virtual))
+# write_thread = threading.Thread(target=tap_manager.read_from_TCP, args=(tap_lock, args.current_dir,
+#                                                                         args.file_from_host))
+#
+# read_thread.start()
+# write_thread.start()
--- mytuntap.py
+++ mytuntap.py
@@ -33,41 +33,39 @@ class TAP_Manager:
         subprocess.check_call(f'ifconfig tap0 {self.src_ip} pointopoint {self.dst_ip} up', shell=True)
         return self.tun_in
 
-    def read_from_TCP(self, tap_lock, current_dir, file_path):
+    def read_from_TCP(self, current_dir, file_path):
         path_dir = os.path.join(current_dir, file_path)
 
-        while True:
+        try:
+            from_TCP = os.read(self.tun_in.fileno(), 2048)
+        except OSError as e:
+            print(bcolors.FAIL + f"Error writing to tap: {e}" + bcolors.ENDC)
+        else:
+            with open(path_dir, 'ab+') as file:
+                # tap_lock.acquire()
+                file.write(from_TCP)
+                # tap_lock.release()
+                print(bcolors.OKGREEN + f'Записанные данные в {path_dir}: ' + bcolors.ENDC,
+                      ''.join('{:02x} '.format(x) for x in from_TCP))
+                #time.sleep(0.2)
+
+
+    def read_from_file(self, current_dir, file_path):
+        path_dir = os.path.join(current_dir, file_path)
+
+        with open(path_dir, 'rb+') as file:
+            content = file.read()
+            file.seek(0)
+            file.truncate()
+
+        if content:
             try:
-                from_TCP = os.read(self.tun_in.fileno(), 2048)
+                print(bcolors.WARNING + f'Прочитанные данные из {path_dir}:' + bcolors.ENDC,
+                      ' '.join('{:02x}'.format(x) for x in content))
+                # tap_lock.acquire()
+                os.write(self.tun_in.fileno(), bytes(content))
             except OSError as e:
                 print(bcolors.FAIL + f"Error writing to tap: {e}" + bcolors.ENDC)
-            else:
-                with open(path_dir, 'ab+') as file:
-                    tap_lock.acquire()
-                    file.write(from_TCP)
-                    tap_lock.release()
-                    print(bcolors.OKGREEN + f'Записанные данные в {path_dir}: ' + bcolors.ENDC,
-                          ''.join('{:02x} '.format(x) for x in from_TCP))
-                    time.sleep(0.2)
-
-
-    def read_from_file(self, tap_lock, current_dir, file_path):
-        path_dir = os.path.join(current_dir, file_path)
-
-        while True:
-            with open(path_dir, 'rb+') as file:
-                content = file.read()
-                file.seek(0)
-                file.truncate()
-
-            if content:
-                try:
-                    print(bcolors.WARNING + f'Прочитанные данные из {path_dir}:' + bcolors.ENDC,
-                          ' '.join('{:02x}'.format(x) for x in content))
-                    tap_lock.acquire()
-                    os.write(self.tun_in.fileno(), bytes(content))
-                except OSError as e:
-                    print(bcolors.FAIL + f"Error writing to tap: {e}" + bcolors.ENDC)
-                finally:
-                    tap_lock.release()
-            time.sleep(0.2)
+            # finally:
+                # tap_lock.release()
+        #time.sleep(0.2)
