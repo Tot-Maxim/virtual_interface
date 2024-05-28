ef214276fed2fab5c2ce9857fff0e2304e06697f
oem <boom.rezonans@yandex.ru>
2024-05-28 11:12:18 +0300

    rasberry pi

--- ser_write_pi.py
+++ ser_write_pi.py
@@ -1,26 +1,18 @@
-#!/usr/bin/env python3
 import serial
 import time
 
-port_write = '/dev/ttyACM0'
+size = 0
+data_to_send = bytearray()
+for i in range(3000):
+    data_to_send.append(i % 256)
 
-baud_rate = 115200  # Specify the baud rate
+while True:
+    port_write = '/dev/ttyACM0'
+    baud_rate = 115200  # Specify the baud rate
+    ser_write = serial.Serial(port_write, baud_rate)
+    data = ser_write.write(data_to_send)
+    size += data
+    print(f'Write {size} to {port_write}')
+    ser_write.close()
 
-ser_wrtie = serial.Serial(port_write, baud_rate)
 
-data_to_send = b''
-
-for i in range(3):
-    data_to_send += bytes([i])
-
-try:
-    while True:
-        data = ser_wrtie.write(data_to_send)
-        print(f'Write {data_to_send} to {port_write}')
-        time.sleep(0.2)
-
-except KeyboardInterrupt:
-    ser_wrtie.close()
-    print('Serial port is closed')
-except Exception as e:
-    print(f"Error: {e}")
